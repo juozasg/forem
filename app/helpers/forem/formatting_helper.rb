378dad845ef91fbda9163019c7ef359f41d7cfc0
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-01-20 11:18:30 -0800

    blockquote post quoting has a <br/> now to make it work with visual editors

--- app/helpers/forem/formatting_helper.rb
+++ app/helpers/forem/formatting_helper.rb
@@ -13,7 +13,7 @@ module Forem
       if Forem.formatter && Forem.formatter.respond_to?(:blockquote)
         Forem.formatter.blockquote(as_sanitized_text(text)).html_safe
       else
-         "<blockquote>#{(h(text))}</blockquote>\n\n".html_safe
+         "<blockquote>#{(h(text))}</blockquote>\n<br/>\n".html_safe
       end
     end
 
