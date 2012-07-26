d1232e7df438320dc2f8e715bb21a39388f23451
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-07-26 13:36:50 -0700

    link_to for last post in topics view will jump to correct page

--- app/helpers/forem/topics_helper.rb
+++ app/helpers/forem/topics_helper.rb
@@ -1,8 +1,14 @@
 module Forem
   module TopicsHelper
     def link_to_latest_post(post)
+      topic = post.topic
+      post_page = (topic.posts.index(post) / topic.posts.per_page) + 1
+
+      link_params = {:anchor => "post-#{post.id}"}
+      link_params[:page] = post_page if post_page > 1
+
       text = "#{time_ago_in_words(post.created_at)} #{t("ago_by")} #{post.user}"
-      link_to text, forum_topic_path(post.topic.forum, post.topic, :anchor => "post-#{post.id}")
+      link_to text, forum_topic_path(post.topic.forum, post.topic, link_params)
     end
   end
 end
